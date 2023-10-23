b8f400709c4b3f8ac1fe54464220c967cd9e07d4
lenchq <tirr06@gmail.com>
2023-10-23 22:41:25 +0500

    move

---
+++ src/lib/components/NavIcon.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { page } from "$app/stores"
+
+    export let icon: any
+    export let href: string
+    export let title: string
+    export let fill: boolean = false;
+    // export let selected: boolean;
+
+    $: selected = $page.route.id === href
+</script>
+
+<a
+    {href}
+    class="block"
+>
+    <div
+        class="p-2 flex flex-col gap-y-2 place-content-center place-items-center justify-between"
+    >
+        <div
+            class="nav-icon fill-transparent stroke-gray"
+            class:fill={selected && fill}
+            class:selected
+        >
+            {@html icon}
+        </div>
+        <span
+            class="nav-text text-gray !font-[300]"
+            class:selected
+        >
+            {title}
+        </span>
+    </div>
+</a>
+
+<style lang="scss">
+    .selected {
+        color: theme("colors.orange-accent");
+        stroke: theme("colors.orange-accent");
+    }
+    .fill {
+        fill: theme("colors.orange-accent");
+    }
+    
+</style>
--- src/routes/app/+layout@.svelte
+++ src/routes/app/+layout@.svelte
@@ -1,1 +1,57 @@
-<slot/>
+<script lang="ts">
+    import { onMount } from "svelte"
+    import { formData } from "../../lib/stores/boardingStore"
+    import { get } from "svelte/store"
+    import search from "$lib/assets/icons/search.svg?raw"
+    import home from "$lib/assets/icons/home.svg?raw"
+    import fridge from "$lib/assets/icons/fridge.svg?raw"
+    import profile from "$lib/assets/icons/profile.svg?raw"
+    import NavIcon from "$lib/components/NavIcon.svelte"
+    import { page } from "$app/stores"
+
+    let meals: string[]
+
+    onMount(() => {
+        meals = get(formData.meals)
+    })
+</script>
+
+<section
+    class="min-h-[calc(100vh-80px)] flex flex-col place-content-start place-items-center p-4 gap-y-[22px]"
+>
+    {get(formData.priceRange)}
+    {get(formData.breakfast)}
+    {get(formData.dinner)}
+    {get(formData.tea)}
+    {get(formData.tea)}
+    {get(formData.tea)}
+
+    <div>
+        {$page.route.id}
+        <slot />
+    </div>
+</section>
+<nav class="place-self-end h-20 place-content-center flex gap-x-3 max-w-full">
+    <NavIcon
+        icon={home}
+        fill={true}
+        href="/app"
+        title="Главная"
+    />
+    <NavIcon
+        icon={search}
+        href="/app/explore"
+        title="Поиск"
+    />
+    <NavIcon
+        icon={fridge}
+        href="/app/fridge"
+        title="Холодильник"
+    />
+    <NavIcon
+        fill={true}
+        icon={profile}
+        href="/app/profile"
+        title="Профиль"
+    />
+</nav>
